f2c8b6313a170410b2ee1c7b8f80beb3b214431d
Daniel Benton <dob@Daniels-MacBook.local>
2023-06-14 14:44:50 +0100

    Hello world on bare metal

--- zephyrRS/src/main.rs
+++ zephyrRS/src/main.rs
@@ -3,11 +3,22 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function when compiled  (needs to called start)
 pub extern "C" fn _start() -> ! { // ! sets a diverging return value 
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i,&byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2+1) = 0xb;    
+        }
+    }
+
     loop {}
 }
 
