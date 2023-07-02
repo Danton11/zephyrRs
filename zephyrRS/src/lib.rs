248d77d6536b1d09048f46ac91754fca422e4397
Daniel Benton <dob@Daniels-MacBook.local>
2023-07-02 11:10:10 +0100

    Added test library/framework, also prints QEMU output to terminal instead of gui

---
+++ zephyrRS/src/lib.rs
@@ -0,0 +1,87 @@
+#![no_std]
+#![cfg_attr(test,no_main)]
+#![feature(custom_test_frameworks)] // enables custom test frameworks
+#![test_runner(crate::test_runner)] // specifies the test runner
+#![reexport_test_harness_main = "test_main"] // re-exports the test harness main as "test_main"
+
+use core::panic::PanicInfo;
+
+pub mod serial;
+pub mod vga_buffer;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+
+impl<T> Testable for T 
+where T: Fn(),{
+    fn run(&self){
+        serial_print!("{}...\t", core::any::type_name::<T>()); // type_name gets the name of the function/test being run
+        self(); //invoke the test
+        serial_println!("[ok]");
+    }
+}
+
+//custom test_runner
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len()); // number of tests to be run
+    
+    for test in tests { // run each test
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> !{
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n",info);
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode){
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop{}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+// simple vga test
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output"); 
+}
+
+
+// simple vga test
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200{
+        println!("test_println_many output"); 
+    }
+}
+
+
--- zephyrRS/src/main.rs
+++ zephyrRS/src/main.rs
@@ -1,46 +1,51 @@
-#![no_std]
-#![allow(non_snake_case)]
-#![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
+#![no_std] // Tells the Rust compiler not to link the std library (C)
+#![allow(non_snake_case)] 
+#![no_main] // Tells the Rust compiler that it does not need to adhere to the std C runtime (i.e calling main)
+#![feature(custom_test_frameworks)] // enables custom test frameworks
+#![test_runner(zephyrRS::test_runner)] // specifies the test runner
+#![reexport_test_harness_main = "test_main"] // re-exports the test harness main as "test_main"
 
 use core::panic::PanicInfo;
 //use core::fmt::Write;
 
-mod vga_buffer;
+mod vga_buffer; // imports the custom vga module
+mod serial;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}",_info);
-    loop {}
-} 
-
-
+// entry point
 #[no_mangle] // don't mangle the name of this function when compiled  (needs to called start)
 pub extern "C" fn _start() -> ! { // ! sets a diverging return value 
     // this function is the entry point, since the linker looks for a function
 
     println!("Hello World{}","!"); // this println! uses the macro defined in vga_buffer.rs
 
+    // if the cfg attribute 'test' is set, call the function test_main
     #[cfg(test)]
     test_main();
 
     loop {}
 }
 
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info); // prints panic info
+    loop {} 
+} 
+
+// our panic handler in test mode 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    zephyrRS::test_panic_handler(info)
 }
 
+
+// practice test_case
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
+    assert_eq!(1, 1); // assertion 
 }
+
+
