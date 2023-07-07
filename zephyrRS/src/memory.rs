1d4e997db3028beebfa59cd1b8d6a1f2f09fb9b1
Daniel Benton <dob@Daniels-MacBook.local>
2023-07-07 18:32:19 +0100

    Added more comments to memory.rs

--- zephyrRS/src/memory.rs
+++ zephyrRS/src/memory.rs
@@ -6,6 +6,23 @@ use x86_64::{
     PhysAddr, VirtAddr,
 };
 
+
+
+
+
+
+
+
+
+//Remember that page tables are used by the MMU (Memory Management Unit) to translate virtual addresses to physical addresses. When a program accesses an address, it provides a virtual address, which the MMU then translates to a physical address. The physical address is then used to access the actual data in memory. The mapping from virtual to physical addresses is done through a set of hierarchical page tables.
+
+
+
+
+
+///- `init`: This function initializes an `OffsetPageTable` which can translate virtual addresses to physical addresses and vice versa. It requires the `physical_memory_offset` which indicates the difference between the physical and virtual address of a page.
+
+
 /// Initialize a new OffsetPageTable.
 ///
 /// This function is unsafe because the caller must guarantee that the
@@ -16,6 +33,9 @@ pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static>
     let level_4_table = active_level_4_table(physical_memory_offset);
     OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
+
+
+///- `active_level_4_table`: This function returns a mutable reference to the level 4 page table currently active in the CPU. It reads the value from the CR3 register (which contains the physical address of the active level 4 table) and converts it to the equivalent virtual address using the provided `physical_memory_offset`.
 
 /// This function is unsafe because the caller must guarantee that the
 /// complete physical memory is mapped to virtual memory at the passed
@@ -32,6 +52,9 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
 
     &mut *page_table_ptr // unsafe
 }
+
+
+///- `create_example_mapping`: This function maps a provided page to the frame at the physical address `0xb8000` with the `PRESENT` and `WRITABLE` flags. It's used for testing purposes and not safe to use in a real kernel since the frame at `0xb8000` might be already in use.
 
 /// Creates an example mapping for the given page to frame `0xb8000`.
 pub fn create_example_mapping(
@@ -51,6 +74,8 @@ pub fn create_example_mapping(
     map_to_result.expect("map_to failed").flush();
 }
 
+
+///- `EmptyFrameAllocator`: This is a dummy frame allocator that never allocates any frames. It's used when you don't have a physical memory manager yet or for testing purposes.
 /// A FrameAllocator that always returns `None`.
 pub struct EmptyFrameAllocator;
 
@@ -66,7 +91,12 @@ pub struct BootInfoFrameAllocator {
     next: usize,
 }
 
+
+///- `BootInfoFrameAllocator`: This frame allocator uses the memory map provided by the bootloader to track which frames are available for allocation. It filters out all memory regions that are marked as `USABLE` and provides an iterator over all usable frames. Each time a frame is allocated, it increments the `next` index to keep track of the next frame to allocate.
+
 impl BootInfoFrameAllocator {
+
+    ///- `init`: This method initializes the `BootInfoFrameAllocator`. It is marked as `unsafe` because the caller must ensure that the provided memory map is correct.
     /// Create a FrameAllocator from the passed memory map.
     ///
     /// This function is unsafe because the caller must guarantee that the passed
@@ -79,6 +109,8 @@ impl BootInfoFrameAllocator {
         }
     }
 
+
+    ///- `usable_frames`: This method returns an iterator over all frames marked as `USABLE` in the memory map. It does this by iterating over all regions in the memory map, filtering out regions not marked as `USABLE`, and then converting each region's address range to a list of frame addresses.
     /// Returns an iterator over the usable frames specified in the memory map.
     fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
         // get usable regions from memory map
@@ -93,6 +125,8 @@ impl BootInfoFrameAllocator {
     }
 }
 
+
+///- `allocate_frame`: This method allocates a new frame of memory by returning the next usable frame from the memory map. After each allocation, it increments the `next` index to point to the next usable frame.
 unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
         let frame = self.usable_frames().nth(self.next);
